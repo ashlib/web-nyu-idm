a71e091421d5fb207629835d03cb32fe8fc069a4
thegiggler <58568758+ashlib@users.noreply.github.com>
2020-03-20 23:44:09 -0400

    finishing first mobile setting

--- object-meditation/script.js
+++ object-meditation/script.js
@@ -15,18 +15,18 @@ $elements.each(function(i) {
   });
 });
 
-setTimeout(function() { // start a delay
-  var fade = document.getElementById("fade"); // get required element
-  fade.style.opacity = 1; // set opacity for the element to 1
-  var timerId = setInterval(function() { // start interval loop
-    var opacity = fade.style.opacity; // get current opacity
-    if (opacity == 0) { // check if its 0 yet
-      clearInterval(timerId); // if so, exit from interval loop
-    } else {
-      fade.style.opacity = opacity - 0.001; // else remove 0.05 from opacity
-    }
-  }, 100); // run every 0.1 second
-}, 5000); // wait to run after 5 seconds
+// setTimeout(function() { // start a delay
+//   var fade = document.getElementById("fade"); // get required element
+//   fade.style.opacity = 1; // set opacity for the element to 1
+//   var timerId = setInterval(function() { // start interval loop
+//     var opacity = fade.style.opacity; // get current opacity
+//     if (opacity == 0) { // check if its 0 yet
+//       clearInterval(timerId); // if so, exit from interval loop
+//     } else {
+//       fade.style.opacity = opacity - 0.001; // else remove 0.05 from opacity
+//     }
+//   }, 100); // run every 0.1 second
+// }, 5000); // wait to run after 5 seconds
 
 $("#shock").click(function() {
   $("#home").css({
