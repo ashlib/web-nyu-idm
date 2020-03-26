5143ea891a5a8bfef4878151cb471f77145c9b81
thegiggler <58568758+ashlib@users.noreply.github.com>
2020-03-26 15:29:46 -0400

    I have console.log array items

--- javascript-playground/challenge.js
+++ javascript-playground/challenge.js
@@ -1,42 +1,46 @@
-// //Welcome to the in-class challenge!
-//
-//
-// let myAlphabet = ['A', 'B', 'C', 'D','E','F','G'];
-//
-// // 1. What is the length of the array?
-//
-// myAlphabet.length
-//
-// // push your edits to github!!
-//
-// // 2. Write a function called myAlphabetLength which console.logs the length of the array
-// // push your edits to github!!
-//
-// function myAlphabetLength(){
-//   myAlphabet.length;
-//   console.log(myAlphabet.length);
-// }
-//
-// myAlphabetLength();
-//
-// // 3. Within the function also use an if-conditional statement that checks if the number of items within the array are less than 4
-// // push your edits to github!!
-//
-// if(myAlphabet.length < 4) {
-//   console.log("number of items is less than 4");
-// }
-//   else {
-//     console.log("number of items is greater than 4")
-//   }
-//
-// // 4. Declare and initialize an array called 'colors' with 5 different string values representing the names of 5 colors of your choosing
-// // push your edits to github!!
-//
-// // 5.  Console.log each item in the array
-// // push your edits to github!!
-//
-// // 6. Declare an empty string variable called 'text'. Each time you console.log a color from the above challenge number 2, add your color to this variable.
-// // push your edits to github!!
-//
-// //7. Create a div in the index.html with a unique id called whatever you want. Display the contents of your text variable to this div. Now you should have a list of 5 colors visible on your website.
-// // push your edits to github!!
+//Welcome to the in-class challenge!
+
+
+let myAlphabet = ['A', 'B', 'C', 'D','E','F','G'];
+
+// 1. What is the length of the array?
+
+myAlphabet.length
+
+// push your edits to github!!
+
+// 2. Write a function called myAlphabetLength which console.logs the length of the array
+// push your edits to github!!
+
+function myAlphabetLength(){
+  myAlphabet.length;
+  console.log(myAlphabet.length);
+}
+
+myAlphabetLength();
+
+// 3. Within the function also use an if-conditional statement that checks if the number of items within the array are less than 4
+// push your edits to github!!
+
+if(myAlphabet.length < 4) {
+  console.log("number of items is less than 4");
+}
+  else {
+    console.log("number of items is greater than 4")
+  }
+
+// 4. Declare and initialize an array called 'colors' with 5 different string values representing the names of 5 colors of your choosing
+// push your edits to github!!
+
+let colorsArray = ["red", "orange", "yellow", "green", "blue"];
+
+// 5.  Console.log each item in the array
+// push your edits to github!!
+
+console.log(colorsArray);
+
+// 6. Declare an empty string variable called 'text'. Each time you console.log a color from the above challenge number 2, add your color to this variable.
+// push your edits to github!!
+
+//7. Create a div in the index.html with a unique id called whatever you want. Display the contents of your text variable to this div. Now you should have a list of 5 colors visible on your website.
+// push your edits to github!!
